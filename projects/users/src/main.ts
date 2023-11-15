b311ad303af4fa030cb2eab3683a258e3d31a7ab
Pedro Hurtado <pedrohurt@gmail.com>
2023-11-15 12:08:44 +0000

    pwa

--- projects/users/src/main.ts
+++ projects/users/src/main.ts
@@ -1,6 +1,13 @@
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent, {
+    providers: [provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        })]
+})
 
