147d4caf7aa37b71f568cc9d82b8e856917cf26d
Pedro Hurtado <pedrohurt@gmail.com>
2023-11-15 11:19:30 +0000

    libray multi app angualr

--- projects/users/src/app/app.component.ts
+++ projects/users/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { Component, ElementRef } from '@angular/core';
+import { CoreModule } from 'core';
+
 
 @Component({
   standalone:true,
-  selector: 'app-root', 
+  imports:[CoreModule],
+  selector: 'app-root',   
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'users';
+  constructor(private readonly element:ElementRef){
+    
+  }
+  handlerClick(){
+      this.element.nativeElement.style.setProperty('color', 'blue')
+  }
 }
